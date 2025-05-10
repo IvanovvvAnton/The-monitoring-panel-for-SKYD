d18e9d5d6985fa869b0d829cae67e22fae5ed7e7
Ivanov_Anton <103459290+IvanovvvAnton@users.noreply.github.com>
2025-05-10 13:02:52 +0300

    Create login_form.py

---
+++ login_form.py
@@ -0,0 +1,20 @@
+@app.route('/auth', methods=['POST'])
+def auth():
+    username = request.form['username']
+    password = request.form['password']
+
+    if username != USERNAME and password != PASSWORD:
+        log_to_login("Авторизация", f"Неудачная попытка входа: {username} | Неверный логин и пароль")
+        return render_template('login.html', error="Неверные данные!")
+
+    if username != USERNAME:
+        log_to_login("Авторизация", f"Неудачная попытка входа: {username} | Неверный логин")
+        return render_template('login.html', error="Неверные данные!")
+
+    if password != PASSWORD:
+        log_to_login("Авторизация", f"Неудачная попытка входа: {username} | Неверный пароль")
+        return render_template('login.html', error="Неверные данные!")
+
+    session['username'] = username
+    log_to_login("Авторизация", f"Успешный вход в систему: {username}")
+    return redirect(url_for('home'))
